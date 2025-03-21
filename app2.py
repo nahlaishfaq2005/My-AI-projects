5220631338f83aa23a537ec6a7a9b049d5b036dc
Nahla Ishfaq <nahlaishfaq@gmail.com>
2025-03-21 22:08:06 +0530

    new

--- app2.py
+++ app2.py
@@ -19,7 +19,7 @@ load_dotenv()
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
 #os.environ["GOOGLE_API_KEY"] = "AIzaSyAlBYH03S1F583dVX-5ae8nyTmvIycdd0o"
-st.title("Chat with PDFII!!")
+st.title("🌈Chat with PDFII!!🌈")
 
 # Load PDF
 loader = PyPDFLoader(r"PDFS\yolov9_paper.pdf")
